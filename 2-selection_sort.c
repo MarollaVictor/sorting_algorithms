8a8f0c766829ebb52a9bfb6208d7d2667f9f0aab
MarollaVictor <marollavictorsedky@gmail.com>
2024-10-28 21:28:49 +0300

    selection_sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorting arrayn with
+ *	selection_sorting algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	if (!array || !size)
+		return;
+	for (i = 0; i < size - 1; i ++)
+	{
+		for (j = size - 1, k = i + 1; j > i; j--)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+
+			}
+		}
+		if (array[i] > array[k])
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
+	}
+}
