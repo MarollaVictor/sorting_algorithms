4ab6de7b4f14bf6363654c80308643884efcb1fa
MarollaVictor <marollavictorsedky@gmail.com>
2024-10-28 19:51:42 +0300

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+
+/**
+ * swap - swaps bigger number with
+ *	lesser one in doubly linked list
+ * @a: address of 1st node
+ * @b: address of 2nd node
+ *
+ * Return: void
+ */
+
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+
+/**
+ * insertion_sort_list - insertion sort for
+ *	doubl linked list
+ * @list: address of pointer to h node
+ *
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
+
+		}
+	}
+}
