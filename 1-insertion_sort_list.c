49a71530dd2a9df73b1ad41fe274f7910e5ccd84
FadySabry712 <fadysabry95@yahoo.com>
2024-11-02 07:53:58 -0700

    modidfed 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,49 @@
 #include "sort.h"
 
-/**
- * swap_l - swaps bigger number with
- *	lesser one in doubly linked list
- * @a: address of 1st node
- * @b: address of 2nd node
- *
- * Return: void
- */
-
-void swap_l(listint_t *a, listint_t *b)
-{
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
-}
 
 /**
- * insertion_sort_list - insertion sort for
- *	doubl linked list
- * @list: address of pointer to h node
- *
- * Return: void
+ * insertion_sort_list -  sort a doubly linked list
+ * of integers in ascending order
+ * @list: Dobuly linked list to sort
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i, *j;
+	listint_t *node;
 
-	if (!list || !*list || !(*list)->next)
+	if (list == NULL || (*list)->next == NULL)
 		return;
-	i = (*list)->next;
-	while (i)
+	node = (*list)->next;
+	while (node)
 	{
-		j = i;
-		i = i->next;
-		while (j && j->prev)
+		while ((node->prev) && (node->prev->n > node->n))
 		{
-			if (j->prev->n > j->n)
-			{
-				swap(j->prev, j);
-				if (!j->prev)
-					*list = j;
-				print_list((const listint_t *)*list);
-			}
-			else
-				j = j->prev;
-
+			node = swap_node(node, list);
+			print_list(*list);
 		}
+		node = node->next;
 	}
+}
+/**
+ *swap_node - swap a node for the previous node
+ *@node: node instance
+ *@list: node list
+ *Return: return a pointer to a node
+ */
+listint_t *swap_node(listint_t *node, listint_t **list)
+{
+	listint_t *back = node->prev, *current = node;
+	/*NULL, 19, 48, 9, 71, 13, NULL*/
+
+	back->next = current->next;
+	if (current->next)
+		current->next->prev = back;
+	current->next = back;
+	current->prev = back->prev;
+	back->prev = current;
+	if (current->prev)
+		current->prev->next = current;
+	else
+		*list = current;
+	return (current);
 }
